3cdfab662521e4bce88fc068aaffcbe5d78fdc81
edenyzl2025 <edenyzl2025@student.cis.edu.hk>
2023-02-01 12:51:09 +0800

    edit

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Dateable {
+
+
+import java.util.Scanner;
+
+public class Dateable {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        double age = scanner.nextInt();
+
+
+        double dateAge = 7+age/2;
+        System.out.println(age+" year olds should date somebody who is at least "+ dateAge+" years old! >:(");
+    }
+
 }
